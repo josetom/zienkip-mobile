d26250dfaa3dada858c27c7c864a66702f39b939
jose <josetom92@gmail.com>
2017-11-17 02:13:55 +0530

    handle ionic module in components

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
 
 import { PageTopComponent } from './page-top/page-top';
 
 @NgModule({
 	declarations: [PageTopComponent],
-	imports: [],
+	imports: [IonicModule.forRoot(ComponentsModule)],
 	exports: [PageTopComponent]
 })
 export class ComponentsModule { }
