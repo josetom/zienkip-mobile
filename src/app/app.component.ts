66bc268d286d0ab6c9626f57a8d9e352751d29c7
jose <josetom92@gmail.com>
2017-11-29 16:41:40 +0530

    side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class Beecon {
-  rootPage:any = HomePage;
+	rootPage: any = HomePage;
+	pages: Array<{ title: string, component: any }>;
+	openPage: Function = function(page: any) {
+		console.log(page);
+	};
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+		this.pages = [{ title: "1", component: HomePage }, { title: "2", component: HomePage }, { title: "3", component: HomePage }]
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			splashScreen.hide();
+		});
+	}
 }
